d799728bfef344bb635bd457a62c0f6dad6e63e7
Shaun Inman <shaun@panic.com>
2022-11-19 13:19:34 -0500

    clarify blob text in package-toolchain.sh

--- support/package-toolchain.sh
+++ support/package-toolchain.sh
@@ -4,4 +4,4 @@ cd /opt/
 tar --xz -cvf trimuismart-toolchain.tar.xz trimuismart-toolchain/
 mv trimuismart-toolchain.tar.xz ~/workspace/
 
-echo "copy trimuismart-toolchain.tar.xz in the support folder on the host machine to reduce subsequent calls to 'make shell' or to share as a blob"
+echo "trimuismart-toolchain.tar.xz can be shared as a blob by placing in support before calling 'make shell'"
